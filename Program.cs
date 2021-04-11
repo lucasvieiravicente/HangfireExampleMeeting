dc47909ee2f704faf84f9da3d5fbb83442e13eb1
Lucas Vieira Vicente <lucasvieiravicente1@gmail.com>
2021-04-11 16:14:38 -0300

    adicionado e configurado serilog

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ExemploMeetingHangfire
 {
@@ -13,14 +9,38 @@ namespace ExemploMeetingHangfire
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            ConfigurarLogger();
+
+            try
+            {
+                Log.Information("Iniciando o projeto.");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Aplicação falhou ao iniciar");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void ConfigurarLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                                            .WriteTo.Console()
+                                            .WriteTo.Debug()
+                                            .Enrich.FromLogContext()
+                                            .CreateLogger();
+        }
     }
 }
